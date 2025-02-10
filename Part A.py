d3559ba1371d725d2beabb028f9a9235a8f5a26c
GouveaLuiz <b00165757@mytudublin.ie>
2025-02-10 17:47:30 +0000

    Completed Part A

---
+++ Part A.py
@@ -0,0 +1,15 @@
+#Luiz Gouvea
+#10/02/2025
+#Lab 2 - week 3
+
+#A Program to Print Steps 4 to 6 to the Screen
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+print("Step 4")
+print("Step 5")
+print("Step 6")
+print("Step 7")
+print("Step 8")
+print("Step 9")
