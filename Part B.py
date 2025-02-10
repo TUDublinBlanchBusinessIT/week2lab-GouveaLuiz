4d0ab7e23a3deb6807eefea64d74a411d9f29dc9
GouveaLuiz <b00165757@mytudublin.ie>
2025-02-10 17:55:27 +0000

    Completed Part B

--- Part B.py
+++ Part B.py
@@ -2,16 +2,15 @@
 #10/02/2025
 #Lab 2 - week 3
 
-#A Program to Print Steps 4 to 6 to the Screen
-def printSteps():
-    print("Step 4")
-    print("Step 5")
-    print("Step 6")
+
+def printSteps(a,b):
+    for i in range (a, b):
+        print("Step " +str(i))
     
 print("Step 1")
 print("Step 2")
 print("Step 3")
-printSteps()
+printSteps(4, 6)
 print("Step 7")
 print("Step 8")
 print("Step 9")
